0e833ef8745d540eef990b52acc57cf47f765acb
BHDisrael <israel970104@gmail.com>
2023-09-27 14:28:02 +0100

    Striker

---
+++ src/fantasyfootballsetup/Striker.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fantasyfootballsetup;
+
+/**
+ *
+ * @author aamyr
+ */
+public class Striker extends Player {
+    
+    public Striker(String fname, String lname, String country, int age, String position) {
+        super(fname, lname, country, age, position);
+    }
+    
+}
